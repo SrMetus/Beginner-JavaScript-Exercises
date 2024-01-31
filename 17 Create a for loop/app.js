83a8541c96ac7b3be322a8e7313c00e6d7efecc3
SrMetus <s.gaggero87@gmail.com>
2024-01-30 23:20:02 -0300

    adding style M!

---
+++ 17 Create a for loop/app.js
@@ -0,0 +1,6 @@
+function standardsMaker() {
+    for(let i = 0; i < 300; i++){
+        console.log("I will write questions if I'm stuck");
+    }
+}
+standardsMaker();
---
+++ 18 While loop/app.js
@@ -0,0 +1,11 @@
+function startCounting() {
+	let counter = 101;
+	while (counter > 0) {
+		counter--;
+		console.log(counter);
+	}
+
+	return counter;
+}
+
+startCounting();
